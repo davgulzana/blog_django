275154388573fd541c30d101b8888958bf54cf65
Ohanefk <ohanefk@gmail.com>
2020-12-30 21:54:04 +0600

    fix add post view, added permissions

--- blog/class_views.py
+++ blog/class_views.py
@@ -1,6 +1,6 @@
 from django.views.generic import ListView, DetailView, \
     CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from django.shortcuts import redirect
 
@@ -32,6 +32,12 @@ class PostDetailView(DetailView):
     template_name = 'blog/post_detail.html'
     context_object_name = 'post'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user_has_permission'] = self.request.user.is_authenticated and self.get_object(self.get_queryset()).author == self.request.user
+        return context
+
+
 
 class ViewsMixin:
     def get_context_data(self, **kwargs):
@@ -43,6 +49,11 @@ class ViewsMixin:
         return self.object.get_absolute_url()
 
 
+class UserHasPermissionMixin(UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.is_authenticated and self.get_object(self.get_queryset()).author == self.request.user
+
+
 class AddPostView(LoginRequiredMixin, ViewsMixin, CreateView):
     model = Post
     template_name = 'blog/add_post.html'
@@ -51,18 +62,18 @@ class AddPostView(LoginRequiredMixin, ViewsMixin, CreateView):
     def form_valid(self, form):
         user = self.request.user
         post = form.save(commit=False)
-        post.user = user
+        post.author = user
         post.save()
         return redirect(post.get_absolute_url())
 
 
-class UpdatePostView(ViewsMixin, UpdateView):
+class UpdatePostView(UserHasPermissionMixin, ViewsMixin, UpdateView):
     model = Post
     template_name = 'blog/update_post.html'
     form_class = UpdatePostForm
 
 
-class DeletePostView(DeleteView):
+class DeletePostView(UserHasPermissionMixin, DeleteView):
     model = Post
     template_name = 'blog/delete_post.html'
     success_url = reverse_lazy('home-page')
